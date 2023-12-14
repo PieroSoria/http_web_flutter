5e05ee14bcf6b73c2fb347372690630783dab461
Alex <ysasi0234.soria@gmail.com>
2023-12-14 14:02:43 -0500

    http_web_flutter 3

--- lib/src/myhttpclient.dart
+++ lib/src/myhttpclient.dart
@@ -4,20 +4,19 @@ import 'dart:html' as html;
 import 'package:flutter/material.dart';
 
 class MyHttpClient {
-  Future<Map<String, dynamic>> get(String url) async {
-    final response = await html.HttpRequest.getString(url);
-    return _decodeResponse(response);
-  }
-
   Future<Map<String, dynamic>> post(
       String url, Map<String, String> body) async {
     final response = await html.HttpRequest.postFormData(url, body);
-    return _decodeResponse(response.responseText ?? '');
+    return _decodeResponse(response);
   }
 
-  Map<String, dynamic> _decodeResponse(String response) {
+  Map<String, dynamic> _decodeResponse(html.HttpRequest response) {
     try {
-      return jsonDecode(response);
+      if (response is html.HttpRequest) {
+        return jsonDecode(response.responseText ?? '');
+      } else {
+        throw Exception('Invalid response type: ${response.runtimeType}');
+      }
     } catch (e) {
       debugPrint('Error decoding response: $e');
       throw Exception('Error decoding response: $e');
